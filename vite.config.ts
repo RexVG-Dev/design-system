84aec95c2c47bc4893fb83c69f69b2c570b6fc32
RexVargas <rvargas.itq@gmail.com>
2025-05-19 13:46:34 -0600

    config: set up project to publish as a library

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,45 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    libInjectCss(),
+  ],
   resolve:{
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
       '@styles': path.resolve(__dirname, 'src/styles'),
       '@utils': path.resolve(__dirname, 'src/utils'),
     }
-  }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'legacy',
+        includePaths: [path.resolve(__dirname, 'src')],
+      }
+    }
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'DesignSystem',
+      fileName: (format) => `design-system.${format}.js`,
+      formats: ['es', 'umd'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    cssCodeSplit: false,
+  },
 })
